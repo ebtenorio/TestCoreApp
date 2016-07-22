07751a9ea35577017ca458f970f68d4d63bd0dd6
Eldon B. Tenorio <ebtenorio@yahoo.com>
2016-07-22 13:33:26 +0800

    Test Console-based app for Docker
    Test Console-based app for Docker

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                Console.WriteLine("This is line number " + i.ToString() + ", at " + DateTime.Now);
+            }
+        }
+    }
+}
