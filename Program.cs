e5582d52fd5f398e9b02d12d0bcf3becd29f9337
Eldon B. Tenorio <ebtenorio@yahoo.com>
2016-08-02 14:25:13 +0800

    Changed some lines
    Changed some lines

--- Program.cs
+++ Program.cs
@@ -6,10 +6,7 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            for (int i = 0; i <= 100; i++)
-            {
-                Console.WriteLine("This is line number " + i.ToString() + ", at " + DateTime.Now);
-            }
+            Console.WriteLine("This is another line");
         }
     }
 }
