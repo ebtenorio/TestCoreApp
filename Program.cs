c5aa6d9cb0194a7a47146ac50ed7c92b70da31e5
Eldon B. Tenorio <ebtenorio@yahoo.com>
2016-08-02 14:26:55 +0800

    Added some lines
    Added some lines

--- Program.cs
+++ Program.cs
@@ -6,7 +6,11 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("This is another line");
+            Console.WriteLine("Line 1.");
+            Console.WriteLine("Line 2.");
+            Console.WriteLine("Line 3.");
+            Console.WriteLine("Line 4.");
+            Console.WriteLine("Line 5.");
         }
     }
 }
